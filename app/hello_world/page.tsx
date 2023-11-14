e40aa947ec2b5d07d69af77fc586b006cd14a82f
RyoNinomiya <fcbryo_3098@yahoo.co.jp>
2023-11-14 18:15:48 +0900

    SimpleButtonにprops追加

--- app/hello_world/page.tsx
+++ app/hello_world/page.tsx
@@ -4,9 +4,9 @@ import SimpleButton from '@/components/SimpleButton'
 const HelloWorld: NextPage = () => {
   return (
     <>
-      <h1>Ttile</h1>
+      <h1>Top Pgae</h1>
       <p>content</p>
-      <SimpleButton />
+      <SimpleButton text={'oppapi'} />
     </>
   )
 }
